eb835d72c44b79c101416ed65f8704001bb7646a
Sweets <Sweets@users.noreply.github.com>
2020-07-23 21:39:41 -0700

    Add: vector implementation

--- src/globals.h
+++ src/globals.h
@@ -2,6 +2,21 @@
 
 extern unsigned int loglevel;
 
+typedef struct {
+    void** elements;
+    unsigned int memory;
+    unsigned int size;
+    unsigned int remaining;
+} vector_t;
+
+vector_t *construct_vector(void);
+void *vector_iterator(vector_t*);
+void reset_vector_iterator(vector_t*);
+void push_to_vector(vector_t*, void*);
+void pull_from_vector(vector_t*, unsigned int);
+void *get_from_vector(vector_t*, unsigned int);
+void deconstruct_vector(vector_t*);
+
 void _log(unsigned short, const char*, const char*, const int, char*, ...);
 #define log_fatal(...) _log(1, __FILE__, __func__, __LINE__, __VA_ARGS__)
 #define log_message(...) _log(2, __FILE__, __func__, __LINE__, __VA_ARGS__)
--- src/prism.c
+++ src/prism.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
 #include "globals.h"
 
@@ -9,6 +10,74 @@ unsigned int loglevel = 3;
 
 int main(int argc, char **argv) {
     return window_manager();
+}
+
+/* Helpers */
+
+vector_t *construct_vector() {
+    vector_t *vector = (vector_t*)calloc(1, sizeof(vector_t));
+    vector->memory = 1;
+    vector->size = 0;
+    vector->elements = calloc(1, sizeof(void*));
+    vector->remaining = 0;
+
+    return vector;
+}
+
+void *vector_iterator(vector_t *vector) {
+    if (!vector->remaining) {
+        vector->remaining = vector->size;
+        return NULL;
+    }
+
+    unsigned int index = (vector->size - vector->remaining);
+    vector->remaining--;
+
+    return get_from_vector(vector, index);
+}
+
+void reset_vector_iterator(vector_t *vector) {
+    vector->remaining = vector->size;
+}
+
+void push_to_vector(vector_t *vector, void *data) {
+    if (vector->size == vector->memory) {
+        vector->memory *= 2;
+        vector->elements = realloc(vector->elements,
+            sizeof(void*) * (vector->memory + 1));
+    }
+
+    vector->elements[vector->size++] = (void*)data;
+    vector->remaining++;
+}
+
+void pull_from_vector(vector_t *vector, unsigned int index) {
+    if (index >= vector->size)
+        return;
+
+    for (; index < vector->size; index++)
+        vector->elements[index] = vector->elements[index + 1];
+
+    vector->elements[vector->size--] = NULL;
+    vector->remaining--;
+
+    if ((vector->size * 2) == vector->memory) {
+        vector->memory /= 2;
+        vector->elements = realloc(vector->elements,
+            sizeof(void *) * (vector->memory + 1));
+    }
+}
+
+void *get_from_vector(vector_t *vector, unsigned int index) {
+    if (index >= vector->size)
+        return NULL;
+
+    return vector->elements[index];
+}
+
+void deconstruct_vector(vector_t *vector) {
+    free(vector->elements);
+    free(vector);
 }
 
 void _log(unsigned short level, const char *file, const char *function,
