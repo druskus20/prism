37c43c218305d9237fd23ef0fc3c849df9e82a4f
Sweets <Sweets@users.noreply.github.com>
2020-08-01 19:06:40 -0700

    Add minor IPC parsing

--- src/ipc/parsing.c
+++ src/ipc/parsing.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "parsing.h"
+
+ipc_command_t ipc_get_command(char **args) {
+    char *command = args[0];
+
+    if (!strcmp(command, "-q"))
+        return IPC_CMD_QUIT;
+    else if (!strcmp(command, "-T"))
+        return IPC_CMD_SET_TILE_DIRECTION;
+
+    return IPC_CMD_NULL;
+}
+
+char **ipc_split_input(char *buffer) {
+    char **argv = calloc(1, sizeof(char*));
+    unsigned int argc  = 1;
+    unsigned int index = 0;
+
+    char *token;
+    while ((token = strsep(&buffer, " "))) {
+        if (index + 1 > argc) {
+            argc++;
+            argv = realloc(argv, sizeof(char*) * argc);
+        }
+
+        argv[index] = token;
+        printf("%s\n", argv[index]);
+        index++;
+    }
+
+    return argv;
+}
